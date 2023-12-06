e3c50ef09bc6a5479a0cfc446851f6533274b639
Vinayak Sharma <50984182+Stankloard@users.noreply.github.com>
2023-12-07 01:44:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,12 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
